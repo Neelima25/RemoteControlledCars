e2fe31dd0e728d1a6a14ec8564b3cb37e09b3e9b
Neelima Salesha <s.neelima25@gmail.com>
2018-01-21 00:23:42 +1100

    created direction enums

--- RemoteControlledCars/src/main/java/com/whiteclark/assignment/RemoteControlledCars/RemoteControlledCars.java
+++ RemoteControlledCars/src/main/java/com/whiteclark/assignment/RemoteControlledCars/RemoteControlledCars.java
@@ -9,15 +9,62 @@ public class RemoteControlledCars {
 	}
 
 	public Position move(Position position, String command) {
-		if (position == null)
-			throw new RuntimeException("position object is null");
-		if (command == null)
-			throw new RuntimeException("Command object is null");
+		if (position == null || command == null)
+			throw new RuntimeException("position and command are mandatory");
 		if (position.getX() < 1 || position.getX() > gridX)
 			throw new RuntimeException("x of position is out of range");
 		if (position.getY() < 1 || position.getY() > gridY)
 			throw new RuntimeException("y of position is out of range");
-
+		Direction curr = Direction.N;
+		
+		
+		
+		
 		return position;
 	}
+
+}
+
+enum Direction {
+	N {
+		public Direction getDirection(char command) {
+			if (command == 'L')
+				return W;
+			if (command == 'R')
+				return E;
+			return N;
+		}
+	},
+	E {
+		public Direction getDirection(char command) {
+			if (command == 'L')
+				return N;
+			if (command == 'R')
+				return S;
+			return E;
+		}
+	},
+	W {
+		public Direction getDirection(char command) {
+			if (command == 'L')
+				return S;
+			if (command == 'R')
+				return N;
+			return W;
+		}
+	},
+	S {
+		public Direction getDirection(char command) {
+			if (command == 'L')
+				return E;
+			if (command == 'R')
+				return W;
+			return S;
+		}
+	};
+
+	public Direction getDirection(char command) {
+		return N;
+	}
+
 }
