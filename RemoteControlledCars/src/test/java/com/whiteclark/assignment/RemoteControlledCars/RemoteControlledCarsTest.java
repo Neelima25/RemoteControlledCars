3d1560f8d3392a3543dd93a41e7a19102cc00bb4
Neelima Salesha <s.neelima25@gmail.com>
2018-01-20 22:13:14 +1100

    Starting Test cases

---
+++ RemoteControlledCars/src/test/java/com/whiteclark/assignment/RemoteControlledCars/RemoteControlledCarsTest.java
@@ -0,0 +1,30 @@
+package com.whiteclark.assignment.RemoteControlledCars;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class RemoteControlledCarsTest extends TestCase{
+	
+    public RemoteControlledCarsTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( RemoteControlledCarsTest.class );
+    }
+
+    /**
+     * Test case for a positive scenario
+     */
+    public void testPositiveCaseOne()
+    {
+        
+    }
+
+}
